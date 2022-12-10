5435b516704683236c81f780b5e7941ab9dbd513
Nemantaj Garg <nemantaj@gmail.com>
2022-12-10 10:17:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const dataRoutes = require("./routes/data");
 
 const PORT = process.env.PORT || 3001;
 const corsOptions = {
-  origin: "https://app-pet-client-1.vercel.app",
+  origin: "*",
   credentials: true,
   optionSuccessStatus: 200,
 };
